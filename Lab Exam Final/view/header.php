6e1d6a652036a3c3aa8780068d96fe6bda5db6d1
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-01-14 01:30:04 +0600

    Update header.php

--- Lab Exam Final/view/header.php
+++ Lab Exam Final/view/header.php
@@ -6,6 +6,7 @@
             <table width="100%" id="t1">
                 <tr>
                     <td width="" align="left"><a href="home.php" id="l1">Home</a></td>
+                    <td width="" align="right"><a href="viewBlog.php" id="l1">Blog</a></td>
                     <td width="" align="right"><a href="signin.php" id="l1">Sign In</a></td>
                     <td width="" align="right"><a href="signup.php" id="l1">Sign Up</a></td>
                 </tr>
